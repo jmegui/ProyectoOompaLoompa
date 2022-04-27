4f95419450096bbc7f9cd2b1a7f85079db0aa2ed
jmegui <93380342+jmegui@users.noreply.github.com>
2022-04-27 16:39:23 +0200

    Add files via upload

---
+++ Jugador.js
@@ -0,0 +1,112 @@
+import * as THREE from './libs/three.module.js'
+ 
+class Jugador extends THREE.Object3D {
+  constructor(gui,titleGui) {
+    super();
+    
+    // Se crea la parte de la interfaz que corresponde a la caja
+    // Se crea primero porque otros métodos usan las variables que se definen para la interfaz
+    this.createGUI(gui,titleGui);
+
+    //Valores posicion
+    this.vertical = new THREE.Vector3(0,1,0);
+    this.frente = new THREE.Vector3(1,0,0);
+    this.cantidadAvance = 0.2;
+    this.cantidadGiro = 0.03;
+    
+    this.createCameraPrimeraPersona();
+
+  }
+
+  createCameraPrimeraPersona(){
+
+    this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    // También se indica dónde se coloca
+    this.camera.position.set (0, 5, 0);
+    // Y hacia dónde mira
+    var look = new THREE.Vector3 (1,5,0);
+    this.camera.lookAt(look);
+    this.add (this.camera);
+  }
+
+  avanzar(avanza){
+    var direccion = this.cantidadAvance;
+    if(!avanza) direccion = -this.cantidadAvance;
+
+    this.translateOnAxis (this.frente, direccion);
+  }
+  
+  girar(derecha){
+    var angulo = this.cantidadGiro;
+    if(derecha) angulo = -this.cantidadGiro;
+
+    this.rotateOnAxis (this.vertical, angulo);
+  }
+
+  getCamera(){
+    return this.camera;
+  }
+  
+  createGUI (gui,titleGui) {
+    // Controles para el tamaño, la orientación y la posición de la caja
+    this.guiControls = {
+      sizeX : 1.0,
+      sizeY : 1.0,
+      sizeZ : 1.0,
+      
+      rotX : 0.0,
+      rotY : 0.0,
+      rotZ : 0.0,
+      
+      posX : 0.0,
+      posY : 0.0,
+      posZ : 0.0,
+      
+      // Un botón para dejarlo todo en su posición inicial
+      // Cuando se pulse se ejecutará esta función.
+      reset : () => {
+        this.guiControls.sizeX = 1.0;
+        this.guiControls.sizeY = 1.0;
+        this.guiControls.sizeZ = 1.0;
+        
+        this.guiControls.rotX = 0.0;
+        this.guiControls.rotY = 0.0;
+        this.guiControls.rotZ = 0.0;
+        
+        this.guiControls.posX = 0.0;
+        this.guiControls.posY = 0.0;
+        this.guiControls.posZ = 0.0;
+      }
+    } 
+    
+    // Se crea una sección para los controles de la caja
+    var folder = gui.addFolder (titleGui);
+    // Estas lineas son las que añaden los componentes de la interfaz
+    // Las tres cifras indican un valor mínimo, un máximo y el incremento
+    // El método   listen()   permite que si se cambia el valor de la variable en código, el deslizador de la interfaz se actualice
+    folder.add (this.guiControls, 'sizeX', 0.1, 5.0, 0.1).name ('Tamaño X : ').listen();
+    folder.add (this.guiControls, 'sizeY', 0.1, 5.0, 0.1).name ('Tamaño Y : ').listen();
+    folder.add (this.guiControls, 'sizeZ', 0.1, 5.0, 0.1).name ('Tamaño Z : ').listen();
+    
+    folder.add (this.guiControls, 'rotX', 0.0, Math.PI/2, 0.1).name ('Rotación X : ').listen();
+    folder.add (this.guiControls, 'rotY', 0.0, Math.PI/2, 0.1).name ('Rotación Y : ').listen();
+    folder.add (this.guiControls, 'rotZ', 0.0, Math.PI/2, 0.1).name ('Rotación Z : ').listen();
+    
+    folder.add (this.guiControls, 'posX', -20.0, 20.0, 0.1).name ('Posición X : ').listen();
+    folder.add (this.guiControls, 'posY', 0.0, 10.0, 0.1).name ('Posición Y : ').listen();
+    folder.add (this.guiControls, 'posZ', -20.0, 20.0, 0.1).name ('Posición Z : ').listen();
+    
+    folder.add (this.guiControls, 'reset').name ('[ Reset ]');
+  }
+  
+  update () {
+    // Con independencia de cómo se escriban las 3 siguientes líneas, el orden en el que se aplican las transformaciones es:
+    // Primero, el escalado
+    // Segundo, la rotación en Z
+    // Después, la rotación en Y
+    // Luego, la rotación en X
+    // Y por último la traslación
+  }
+}
+
+export { Jugador };
